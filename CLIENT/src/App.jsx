2d1b1d170f332f63bdb3f7333fcbd5bddfcfeaf4
Suman Ranjan <sumanranjan17291729@gmail.com>
2025-08-06 17:20:37 +0530

    Refactor AddCar component to include state management for car details and image; implement form structure for car listing.

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,45 +1,42 @@
-import { useState } from "react"
-import { Navbar } from "./components/Navbar"
-import { Route, Routes, useLocation } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { Cars } from "./pages/Cars"
-import { CarDetails } from "./pages/CarDetails"
-import MyBookings from "./pages/MyBookings"
-import { Footer } from "./components/Footer"
-import { Layout } from './pages/owner/Layout'
-import { AddCar } from "./pages/owner/AddCar"
-import { DashBoard } from "./pages/owner/Dashboard"
-import { ManageBookings } from "./pages/owner/ManageBookings"
-import { ManageCars } from "./pages/owner/ManageCars"
+import { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Cars } from "./pages/Cars";
+import { CarDetails } from "./pages/CarDetails";
+import MyBookings from "./pages/MyBookings";
+import { Footer } from "./components/Footer";
+import { Layout } from "./pages/owner/Layout";
+import { AddCar } from "./pages/owner/AddCar";
+import { DashBoard } from "./pages/owner/Dashboard";
+import { ManageBookings } from "./pages/owner/ManageBookings";
+import { ManageCars } from "./pages/owner/ManageCars";
 
-  export const App = () => {
-    const location = useLocation()
-    // eslint-disable-next-line no-unused-vars
-    const[showLogin , setShowLogin] =  useState(false)
-    const isOwnerPath = location.pathname.startsWith('/owner')
+export const App = () => {
+  const location = useLocation();
+  // eslint-disable-next-line no-unused-vars
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = location.pathname.startsWith("/owner");
 
-    return (
-      <>  
-        {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+  return (
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/cars" element={<Cars/>} />
-          <Route path="/car-details/:id" element={<CarDetails/>} />
-          <Route path="/my-bookings" element={<MyBookings/>} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
 
-          <Route path="/owner" element={<Layout />}> 
-            <Route index element={<DashBoard />}/>
-            <Route path="add-car" element={<AddCar />}/>
-            <Route path="manage-cars" element={<ManageCars />}/>
-            <Route path="manage-bookings" element={<ManageBookings />}/>
-          </Route>
-          
-        </Routes>
-        
-        {!isOwnerPath && <Footer/>}
-        
-      </>
-    )
-  }
- 
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<DashBoard />} />
+          <Route path="add-car" element={<AddCar />} />
+          <Route path="manage-cars" element={<ManageCars />} />
+          <Route path="manage-bookings" element={<ManageBookings />} />
+        </Route>
+      </Routes>
+
+      {!isOwnerPath && <Footer />}
+    </>
+  );
+};
--- CLIENT/src/pages/owner/AddCar.jsx
+++ CLIENT/src/pages/owner/AddCar.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react"
+import { Title } from "../../components/owner/Title";
 
 export const AddCar = () => {
+  const [image , setImage] = useState(null)
+  const [car, setCar] = useState({
+    model: '',
+    brand: '',
+    year: 0,
+    pricePerDay: 0,
+    category: '',
+    transmission: '',
+    fuel_type: '',
+    seating_capacity: 0,
+    location: '',
+    description: ''
+  });
+
+  const onHandleSumbit = async (e) =>{
+    e.preventDefault()
+  }
+
   return (
-    <div>AddCar</div>
+    <div className="px-4 py-10 md:px-10 flex-1">
+      <Title title="Add New Car" subtitle="Fill in details to list a new car for booking, including pricing, availability, and car specifications."/>
+
+      <form onSubmit={onHandleSumbit} className="flex flex-col gap-5 text-gray-500 text-sm mt-6 max-w-xl">
+        {/* Car Image */}
+        
+      </form>
+    </div>
   )
 }
